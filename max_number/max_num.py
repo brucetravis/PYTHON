8ac8d99347302ca0a882ccaa596a1e583965c0f5
Bruce Ambundo <bruceambundo@gmail.com>
2025-02-25 12:54:06 +0300

    maximum number second commit

--- max_number/max_num.py
+++ max_number/max_num.py
@@ -1,26 +1,25 @@
-# # functio to find the maximum number in a list
-# def max_number(lst):
+# function to find the maximum number in a list
+def maximum_num(list):
+    # If the list if empty return None
+    if not list: # If lst is empty 'not lst' is True but If lst not empty, not lst becomes true
+        return None
     
-#     # Check If the list is empty
-#     if not lst:
-#         return None
+    # Initialize the max number with the first value so that we can compare with the first value
+    max_num = list[0]
     
-#     max_num = lst[0] # Initialize the max_num with the first value
-#                      # so that we have something to compare with
-    
-#     # Loop through all the numbers in the list
-#     for num in lst:
-#         # If any number in the list is greater than the initial value
-#         if num > max_num:
-#             # Update the max value with the lastest max_value
-#             max_num = num
+    # Loop through all the numbers in the list
+    for num in list:
+        if num > max_num:
+            # Update the max_num with the lates max_num value
+            max_num = num
             
-#     # return the max value
-#     return max_num
+    # After Iteration, return the max_value
+    return max_num
 
+numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+print(maximum_num(numbers))
 
-# print(max_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-
+# None means no value
 
 # function to calculate the max number
 def maximum_number(lst):
@@ -39,6 +38,13 @@ def maximum_number(lst):
 
 
 print(maximum_number([1, 2, 3, 4, 5]))
-    
-    
+
+
+# function using the max method
+def maximum_number(lst):
+    return max(lst) if lst else None # Return the max value If the list is not empty, otherwise return None
+
+print(maximum_number([1, 2, 3, 4, 5]))
+print(maximum_number([])) # Output: None (for an empty list)
+
     
